1ea86faf8583f2a2cb982fb00d93bde56f45f6f1
Daniel Freund <daniel.f@solaredge.local>
2018-03-20 19:30:44 +0200

    Adding authentication service at client

---
+++ assets/app/auth/auth.service.ts
@@ -0,0 +1,16 @@
+import { HttpHeaders, HttpClient } from "@angular/common/http";
+import { User } from "./user.model";
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class AuthService {
+    private messages: User[] = [];
+    private httpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type':  'application/json'})
+        };
+    
+        
+
+    constructor(private http: HttpClient){}
+    }
